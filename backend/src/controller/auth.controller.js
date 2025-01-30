a5dece0228b9544d52d6783e84967562b2e840f2
xshshahab <mdshahbuddin9504@gmail.com>
2025-01-30 22:32:50 +0530

    profile pic upload with cloudinary

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -1,3 +1,4 @@
+import cloudinary from "../lib/cloudinary.js";
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
@@ -96,4 +97,25 @@ export const logout = (req, res) => {
   }
 };
 
-export const updateProfile = async (req, res) => {};
+export const updateProfile = async (req, res) => {
+  try {
+    const { profilePic } = req.body;
+    const userId = req.user._id;
+
+    if (!profilePic) {
+      return res.status(400).json({ message: "Profile pic is required" });
+    }
+
+    const uploadResponse = await cloudinary.uploader.upload(profilePic);
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { profilePic: uploadResponse.secure_url },
+      { new: true }
+    );
+
+    res.status(200).json({ updatedUser });
+  } catch (error) {
+    console.error("Error in updateProfile controller :", error);
+    return res.status(500).json({ message: "Internal Server ERROR" });
+  }
+};
