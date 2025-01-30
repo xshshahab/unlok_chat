04f78e7a9efe7d49925d4b19c08d311b3b180df5
xshshahab <mdshahbuddin9504@gmail.com>
2025-01-30 22:06:33 +0530

    using cookieparser in protextRoute middelware

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,14 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 const PORT = process.env.PORT || 5001;
 
