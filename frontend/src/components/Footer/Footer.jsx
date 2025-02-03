bb50c4da6e870f51655d072f21e6358286647d63
xshshahab <mdshahbuddin9504@gmail.com>
2025-02-04 00:05:06 +0530

    Dynamically year update into footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 const Footer = () => {
+
+    const currentYear = new Date().getFullYear();
+
     return (
         <footer className="bg-[#1e1e2e] py-3 text-center">
             <p className="text-[#a6e3a1]">
-                © 2025 Unlok Chat. Follow us on{" "}
-                <a target='_blank' href="https://x.com/xsh_shahab" className="text-[#ff7a93] hover:underline">Twitter</a> and{" "}
-                <a target='_blank' href="https://www.linkedin.com/in/mdshahabuddin82/" className="text-[#ff7a93] hover:underline">LinkedIn</a>.
+                © {currentYear} Unlok Chat. Follow us on{" "}
+                <a target='_blank' rel='noopener noreferrer' href="https://x.com/xsh_shahab" className="text-[#ff7a93] hover:underline">Twitter</a> and{" "}
+                <a target='_blank' rel='noopener noreferrer' href="https://www.linkedin.com/in/mdshahabuddin82/" className="text-[#ff7a93] hover:underline">LinkedIn</a>.
             </p>
         </footer>
-    )
-}
+    );
+};
 
-export default Footer
+export default Footer;
