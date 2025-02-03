0652e673852a55e38f48a126e8b30cf8cf589970
xshshahab <mdshahbuddin9504@gmail.com>
2025-02-04 00:02:52 +0530

    Footer updated

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -5,8 +5,8 @@ const Footer = () => {
         <footer className="bg-[#1e1e2e] py-3 text-center">
             <p className="text-[#a6e3a1]">
                 © 2025 Unlok Chat. Follow us on{" "}
-                <a href="#" className="text-[#ff7a93] hover:underline">Twitter</a> and{" "}
-                <a href="#" className="text-[#ff7a93] hover:underline">LinkedIn</a>.
+                <a target='_blank' href="https://x.com/xsh_shahab" className="text-[#ff7a93] hover:underline">Twitter</a> and{" "}
+                <a target='_blank' href="https://www.linkedin.com/in/mdshahabuddin82/" className="text-[#ff7a93] hover:underline">LinkedIn</a>.
             </p>
         </footer>
     )
